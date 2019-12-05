7001989fb85e6166ed2a9099afdfc23f76609382
ninjaaung <ninja.aung02@gmail.com>
2019-12-04 18:38:34 -0800

    Random Lines added

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from colorama import Fore, Back, Style
 
 
 
+mypoem = '''
+The Moving Finger writes; and, having writ, Moves on:
+nor all thy Piety nor Wit. Shall lure it back to cancel half a Line, 
+Nor all thy Tears wash out a Word of it.
+'''
+
 
 
 
@@ -28,26 +34,45 @@ choice = input("Choose a number 1 - 3: ")
 
 
 
-def main(option):
+def lines_printed_random(words):
+    '''will randomly select lines from a list of strings and print them out in random order. 
+    Repeats are okay and the number of lines printed should be equal to the original number of lines in the poem'''
+    random_lines = len(words)
+    for i in range(len(words)):
+        line_index = random.randint(1, random_lines-1)
+        print(words[line_index] + "\n")
+
+
+
+
+def main(option, poem):
     if option == "1":
         print("not available yet")
     elif option == "2":
         print("asd")
     elif option == "3":
-        print("dsa")
+
+        print(poem + "\n")
+        print(Fore.RED + "Reversed ----> \n" + Fore.RESET)
+
+        lines = poem.split("\n")
+        lines.reverse()
+
+        for poem in lines:
+            print(poem + "\n")
+
+        print(Fore.RED + "Mixed up ----> \n" + Fore.RESET)
+        lines_printed_random(lines)
+
+
     else:
         print("That's not a option goodbye")
 
 
 
 
-    # global lines
-    # lines = poem.split("\n")
-    # lines.reverse()
-
-    # for poem in lines:
-    #     print(poem)
 
 
 
-main(choice)
+
+main(choice, mypoem)
