e49493cab0f54642d1f53d61d4403fbdaac0a8b7
ninjaaung <ninja.aung02@gmail.com>
2019-12-04 18:18:28 -0800

    Choice list

--- app.py
+++ app.py
@@ -18,17 +18,36 @@ __________
 
 os. system('clear')
 print("_" * 55 + Fore.BLUE + banner + Fore.RESET + "_" * 55)
+print('''
+would you like to:
+1. Input your poem directly in
+2. Read from a file
+3. Read the poem already in the code
+''')
+choice = input("Choose a number 1 - 3: ")
 
 
 
-def options():
-    return
+def main(option):
+    if option == "1":
+        print("not available yet")
+    elif option == "2":
+        print("asd")
+    elif option == "3":
+        print("dsa")
+    else:
+        print("That's not a option goodbye")
 
 
-def main(poem):
-    global lines
-    lines = poem.split("\n")
-    lines.reverse()
 
-    for poem in lines:
-        print(poem)
+
+    # global lines
+    # lines = poem.split("\n")
+    # lines.reverse()
+
+    # for poem in lines:
+    #     print(poem)
+
+
+
+main(choice)
