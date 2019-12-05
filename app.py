36ab2d78611302b11efeafd4e744b5b6d2b9ed12
ninjaaung <ninja.aung02@gmail.com>
2019-12-04 17:42:43 -0800

    Reversed lines

---
+++ app.py
@@ -0,0 +1,9 @@
+testpoem = '''The Moving Finger writes; and, having writ, Moves on: 
+nor all thy Piety nor Wit. Shall lure it back to cancel half a Line, 
+Nor all thy Tears wash out a Word of it.'''
+
+
+lines = testpoem.split("\n")
+lines.reverse
+
+print(lines)
